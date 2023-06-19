24c3dad977e14a08cd8f1e2f1f4dc717de798eb5
prithvijedipatil <prithvipatil09@gmail.com>
2023-06-19 15:34:31 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,18 @@
+
+
+
 function highlight() {
     //Write your code here
-
+const bold = document.getElementsByTagName("strong");
+	bold.style.color="green";
 
 }
 
 
 function return_normal() {
     //Write your code here
+const bold = document.getElementsByTagName("strong");
+	bold.style.color="black";
 
     
 }
